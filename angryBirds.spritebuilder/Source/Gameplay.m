6f84f94c3ebaaf4a7b878ebcb365d12fb5398c10
Franco Meola <franco.meola@icloud.com>
2014-06-15 21:01:26 -0300

    Implementación del movimiento de la cámara.

--- angryBirds.spritebuilder/Source/Gameplay.m
+++ angryBirds.spritebuilder/Source/Gameplay.m
@@ -8,6 +8,8 @@
 
 #import "Gameplay.h"
 
+static const float MIN_SPEED = 5.f;
+
 @implementation Gameplay {
     CCPhysicsNode * _physicsNode;
     CCNode * _catapultArm;
@@ -18,6 +20,7 @@
     int triesCount;
     OALSimpleAudio * sounds;
     CCLabelBMFont * _scoreLabel;
+    CCAction * _followBird;
 }
 
 - (void)didLoadFromCCB {
@@ -34,30 +37,28 @@
     [sounds playBg:@"background.mp3" loop:YES];
 }
 
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    // Vuelvo la cámara al al lanzador
-    // ¿Cómo se hace?
-}
-
 - (void)launchBird:(id)sender {
     if (triesCount) {
         CCNode * bird = [CCBReader load:@"Bird"];
+        _currentBird = bird;
         // Calculo la rotación en Radianes
         float rotationRadians = CC_DEGREES_TO_RADIANS(_launcher.rotation);
         // Vector para la rotación
         CGPoint directionVector = ccp(sinf(rotationRadians),cosf(rotationRadians));
         CGPoint ballOffset = ccpMult(directionVector, 50);
-        bird.position = ccpAdd(_launcher.position, ballOffset);
-        [_physicsNode addChild:bird];
+        _currentBird.position = ccpAdd(_launcher.position, ballOffset);
+        [_physicsNode addChild:_currentBird];
         self.position = ccp(0, 0);
-        CCActionFollow * follow = [CCActionFollow actionWithTarget:bird worldBoundary:self.boundingBox];
-        [_contentNode runAction:follow];
+//        CCActionFollow * follow = [CCActionFollow actionWithTarget:_currentBird worldBoundary:self.boundingBox];
+//        [_contentNode runAction:follow];
         // Agrego el impulso inicial
         CGPoint force = ccpMult(directionVector, 10000);
-        [bird.physicsBody applyForce:force];
+        [_currentBird.physicsBody applyForce:force];
         triesCount--;
         [sounds playEffect:@"flying.mp3"];
         [_scoreLabel setString: [NSString stringWithFormat:@"%d x", triesCount]];
+        _followBird = [CCActionFollow actionWithTarget:_currentBird worldBoundary:self.boundingBox];
+        [_contentNode runAction:_followBird];
     }
 }
 
@@ -85,6 +86,34 @@
 - (void)back {
     [sounds stopEverything];
     [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"LevelSelector"]];
+}
+
+- (void)update:(CCTime)delta
+{
+    // if speed is below minimum speed, assume this attempt is over
+    if (ccpLength(_currentBird.physicsBody.velocity) < MIN_SPEED){
+        [self nextAttempt];
+        return;
+    }
+    int xMin = _currentBird.boundingBox.origin.x;
+    if (xMin < self.boundingBox.origin.x) {
+        [self nextAttempt];
+        return;
+    }
+    int xMax = xMin + _currentBird.boundingBox.size.width;
+    if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
+        [self nextAttempt];
+        return;
+    }
+}
+
+- (void)nextAttempt {
+    _currentBird = nil;
+    [_contentNode stopAction:_followBird];
+    [_contentNode setPosition:ccp(0, 0)];
+// ¿Por qué oscila la cámara?
+//    CCActionMoveTo * actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0, 0)];
+//    [_contentNode runAction:actionMoveTo];
 }
 
 - (void)reset {
